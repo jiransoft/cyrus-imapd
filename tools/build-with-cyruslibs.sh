b2fd6a5c65a6f216d4b57cb188328ab88e428352
9folders-seungho <seungho.jung@9folders.com>
2024-05-09 10:42:50 +0900

    build-with-cyruslibs.sh: get rid of old ways

--- tools/build-with-cyruslibs.sh
+++ tools/build-with-cyruslibs.sh
@@ -8,16 +8,14 @@ set -e
 : ${CONFIGOPTS:="--enable-jmap --enable-http --enable-calalarmd --enable-unit-tests --enable-replication --enable-nntp --enable-murder --enable-idled --enable-xapian --enable-autocreate --enable-silent-rules --enable-debug-slowio"}
 export LDFLAGS="-L$LIBSDIR/lib/x86_64-linux-gnu -L$LIBSDIR/lib -Wl,-rpath,$LIBSDIR/lib/x86_64-linux-gnu -Wl,-rpath,$LIBSDIR/lib"
 export PKG_CONFIG_PATH="$LIBSDIR/lib/x86_64-linux-gnu/pkgconfig:$LIBSDIR/lib/pkgconfig:\$PKG_CONFIG_PATH"
-export CFLAGS="-g -fPIC -W -Wall -Wextra -Werror -Wwrite-strings"
-export CXXFLAGS="-g -fPIC -W -Wall -Wextra -Werror"
+export CFLAGS="-Wno-unused-parameter -g -O0 -fPIC -W -Wall -Wextra"
+export CXXFLAGS="-g -fPIC -W -Wall -Wextra"
 export PATH="$LIBSDIR/bin:$PATH"
-autoreconf -v -i
+autoreconf -v -i -s
 echo "./configure --prefix=$TARGET $CONFIGOPTS XAPIAN_CONFIG=$LIBSDIR/bin/xapian-config-1.5"
 ./configure --prefix=$TARGET $CONFIGOPTS XAPIAN_CONFIG=$LIBSDIR/bin/xapian-config-1.5
-make lex-fix
-make -j 8
-make -j 8 check
+sudo make
 sudo make install
-sudo make install-binsymlinks
-sudo cp tools/mkimap /usr/local/cyrus/bin/mkimap
-sudo cp tools/mknewsgroups /usr/local/cyrus/bin/mknewsgroups
+#sudo make install-binsymlinks
+#sudo cp tools/mkimap /usr/local/cyrus/bin/mkimap
+#sudo cp tools/mknewsgroups /usr/local/cyrus/bin/mknewsgroups
