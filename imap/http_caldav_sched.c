bc3445e6ac65879b44fb03061c943629c853b324
9folders-seungho <seungho.jung@9folders.com>
2024-09-30 17:59:52 +0900

    http_caldav_sched.c: Change invitation email content

--- imap/http_caldav_sched.c
+++ imap/http_caldav_sched.c
@@ -429,48 +429,10 @@ static int imip_send_sendmail(const char *userid, icalcomponent *ical, const cha
     buf_appendcstr(&msgbuf, "Content-Type: text/plain; charset=utf-8\r\n");
     buf_appendcstr(&msgbuf, "Content-Disposition: inline\r\n");
 
-    if (originator->name && strcasecmp(originator->name, originator->addr))
-        buf_printf(&plainbuf, "You have received %s from %s <%s>\r\n\r\n",
-                msg_type, originator->name, originator->addr);
-    else
-        buf_printf(&plainbuf, "You have received %s from %s\r\n\r\n", msg_type,
-                originator->addr);
-    if (summary) {
-        buf_setcstr(&tmpbuf, summary);
+    if (descrip) {
+        buf_setcstr(&tmpbuf, descrip);
         buf_replace_all(&tmpbuf, "\n", "\r\n" TEXT_INDENT);
-        buf_printf(&plainbuf, "Summary    : %s\r\n", buf_cstring(&tmpbuf));
-    }
-    if (location) {
-        buf_setcstr(&tmpbuf, location);
-        buf_replace_all(&tmpbuf, "\n", "\r\n" TEXT_INDENT);
-        buf_printf(&plainbuf, "Location   : %s\r\n", buf_cstring(&tmpbuf));
-    }
-    buf_printf(&plainbuf, "When       : %s\r\n", when);
-    if (meth == ICAL_METHOD_REPLY) {
-        if (originator->partstat)
-            buf_printf(&plainbuf, "RSVP       : %s\r\n", originator->partstat);
-    }
-    else {
-        const char *prefix;
-
-        if (status) buf_printf(&plainbuf, "Status     : %s\r\n", status);
-
-        for (prefix = "Attendees  : ", recip=recipients; recip; recip=recip->next) {
-            if (recip->name && strcasecmp(recip->name, recip->addr))
-                buf_printf(&plainbuf, "%s* %s <%s>", prefix, recip->name, recip->addr);
-            else
-                buf_printf(&plainbuf, "%s* %s", prefix, recip->addr);
-            if (recip->role) buf_printf(&plainbuf, "\t(%s)", recip->role);
-            buf_appendcstr(&plainbuf, "\r\n");
-
-            prefix = TEXT_INDENT;
-        }
-
-        if (descrip) {
-            buf_setcstr(&tmpbuf, descrip);
-            buf_replace_all(&tmpbuf, "\n", "\r\n" TEXT_INDENT);
-            buf_printf(&plainbuf, "Description: %s\r\n", buf_cstring(&tmpbuf));
-        }
+        buf_printf(&plainbuf, "%s\r\n", buf_cstring(&tmpbuf));
     }
 
     const char *comment = icalcomponent_get_comment(comp);
@@ -507,55 +469,11 @@ static int imip_send_sendmail(const char *userid, icalcomponent *ical, const cha
     }
     else originator->name = originator->addr;
 
-    buf_printf(&msgbuf, "<b>You have received %s from"
-            " <a href=\"mailto:%s\">%s</a></b><p>\r\n",
-            msg_type, originator->addr, originator->name);
-
-    buf_appendcstr(&msgbuf, "<table border cellpadding=5>\r\n");
-    if (summary) {
-        HTMLencode(&tmpbuf, summary);
-        buf_printf(&msgbuf, HTML_ROW, "Summary", buf_cstring(&tmpbuf));
+    if (descrip) {
+        HTMLencode(&tmpbuf, descrip);
+        buf_printf(&msgbuf, "%s<p>\r\n", buf_cstring(&tmpbuf));
     }
-    if (location) {
-        HTMLencode(&tmpbuf, location);
-        buf_printf(&msgbuf, HTML_ROW, "Location", buf_cstring(&tmpbuf));
-    }
-    buf_printf(&msgbuf, HTML_ROW, "When", when);
-    if (meth == ICAL_METHOD_REPLY) {
-        if (originator->partstat)
-            buf_printf(&msgbuf, HTML_ROW, "RSVP", originator->partstat);
-    }
-    else {
-        const char *prefix;
-
-        if (status) buf_printf(&msgbuf, HTML_ROW, "Status", status);
-
-        buf_appendcstr(&msgbuf, "<tr><td><b>Attendees</b></td>");
-        for (prefix = "<td>", recip = recipients; recip; recip = recip->next) {
-            if (recip->name) {
-                HTMLencode(&tmpbuf, recip->name);
-                recip->name = buf_cstring(&tmpbuf);
-            }
-            else recip->name = recip->addr;
-
-            buf_printf(&msgbuf, "%s&#8226; <a href=\"mailto:%s\">%s</a>",
-                       prefix, recip->addr, recip->name);
-            if (recip->role) buf_printf(&msgbuf, " <i>(%s)</i>", recip->role);
-
-            prefix = "\r\n  <br>";
-        }
-        buf_appendcstr(&msgbuf, "</td></tr>\r\n");
-
-        if (descrip) {
-            HTMLencode(&tmpbuf, descrip);
-            buf_printf(&msgbuf, HTML_ROW, "Description", buf_cstring(&tmpbuf));
-        }
-    }
-    if (comment) {
-        HTMLencode(&tmpbuf, comment);
-        buf_printf(&msgbuf, HTML_ROW, "Comment", buf_cstring(&tmpbuf));
-    }
-    buf_printf(&msgbuf, "</table></body></html>\r\n");
+    buf_printf(&msgbuf, "</body></html>\r\n");
 
     /* iCalendar part */
     buf_printf(&msgbuf, "\r\n--%s_A\r\n", boundary);
